b59c1667c78ab2bedfe5520d16c88205ae37f083
Michael Klein <michael.klein.business@gmail.com>
2020-01-03 23:21:03 +0100

    site fix ++

--- site/dist/site.77de5100.js
+++ site/dist/site.77de5100.js
@@ -39895,7 +39895,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35003" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42193" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
