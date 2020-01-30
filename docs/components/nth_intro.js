a884b2a7e124b339d9d0c4538a4eb71c3c8768b4
Michael Klein <michael.klein.business@gmail.com>
2020-01-30 20:28:22 +0100

    version bump, cleanup

--- docs/components/nth_intro.js
+++ docs/components/nth_intro.js
@@ -41,7 +41,7 @@ component('nth-intro', function * () {
                 <li>tagged template literals for templating</li>
                 <li>proxies for change tracking</li>
               </ul>
-              The library is written in typescript and build as es modules. Most parts, including all templating directives, can be loaded on a as-needed basis. In other words, you don't have to to load 50kb of code that you mostly may not need.
+              The library is written in typescript and built as es6 modules. Most parts, including all templating directives, can be loaded on a as-needed basis. In other words, you don't have to to load 50kb of code that you mostly may not need.
             </p>
             <p> 
               Obviously, the features listed above are only available in the latest browsers (that does not include IE11), so you might need to use babel transforms and/or polyfills to support some of them.
